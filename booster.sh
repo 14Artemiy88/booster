5239221ebac6f2c7abc8d82e05cb50dde41f00f8
Artemiy Misyurov <14artemiy88@gmail.com>
2024-05-01 18:25:39 +0500

    add image preview

--- booster.sh
+++ booster.sh
@@ -6,6 +6,13 @@ declare -r YELLOW_COLOR="\e[0;33m"
 declare -r BLUE_COLOR="\e[1;36m"
 declare -r RED_COLOR="\e[1;31m"
 declare -r GRAY_COLOR="\e[38;5;254m"
+declare -r IMG_PATH="/tmp/booster"
+
+bar_size=$(( $(tput cols) - 25 ))
+
+if [ ! -d $IMG_PATH ]; then
+	mkdir $IMG_PATH
+fi
 
 Help() {
     # Display Help
@@ -17,6 +24,20 @@ Help() {
     echo -e "${YELLOW_COLOR}ARGS${NORMAL_COLOR}
     ${GREEN_COLOR}<CHANNEL>${NORMAL_COLOR}    Channel name"
     echo
+}
+
+load() {
+	if [ ! -f "$IMG_PATH/$vid" ]; then
+	
+		cha=" "
+		(( len=$bar_size - ${#title} ))
+    	local v=$(printf "%-${len}s" "$cha")
+    
+		echo -e '\e[2F\n'
+    	echo -en "Loading image...\t${GREEN_COLOR}$2${NORMAL_COLOR}${v// /$cha}"
+	
+		wget -O "$IMG_PATH/$vid" $1 &> /dev/null
+	fi
 }
 
 if [[ -z $1 ]]; then
@@ -31,17 +52,22 @@ i=0
 str=""
 while true; do
 	title=$(jq ".[$i].post.title" <<< $json | tr -d '"')
-	vid=$(jq ".[$i].media" <<< $json | jq '.[0].vid' | tr -d '"')
 	if [[ $title == null ]]; then
 		break;
 	fi
+	vid=$(jq ".[$i].media" <<< $json | jq '.[0].vid' | tr -d '"')
 	if [[ $vid -ne null ]]; then
 		hasAccess=$(jq ".[$i].post.hasAccess" <<< $json)
 		access="${RED_COLOR} 󰅖${NORMAL_COLOR}"
 		if [[ "$hasAccess" == true ]]; then
 			access="${GREEN_COLOR} 󰄬${NORMAL_COLOR}"
+			preview=$(jq ".[$i].media" <<< $json | jq '.[0].preview' | tr -d '"')
+		else
+			preview=$(jq ".[$i].post.teaser" <<< $json | jq '.[0].url' | tr -d '"')
 		fi
-		str="$str\n ${GRAY_COLOR}https://ok.ru/videoembed/$vid${NORMAL_COLOR}|$access|${BLUE_COLOR}$title${NORMAL_COLOR}"
+		load $preview "$title"
+
+		str="$str\n $access|${BLUE_COLOR}$title${NORMAL_COLOR}|${GRAY_COLOR}$vid${NORMAL_COLOR}"
 	fi
 	((i++))
 done
@@ -51,5 +77,5 @@ if [[ $str == "" ]]; then
 	exit 0
 fi
 
-str="  ${YELLOW_COLOR}URL|Free| Title${NORMAL_COLOR}\n$str"
-echo -e "$str" | column --table --separator "|" | fzf --ansi  --header '  CTRL-V Only 󰄬 / CTRL-X Only 󰅖 / CTRL-A All' --header-lines=1 --bind 'enter:become(mpv {1})+abort' --bind "ctrl-v:+transform-query(echo '󰄬 ')" --bind "ctrl-x:+transform-query(echo '󰅖 ')" --bind "ctrl-a:+transform-query(echo )"
+str="  ${YELLOW_COLOR}Free| Title| Id${NORMAL_COLOR}\n$str"
+echo -e "$str" | column --table --separator "|" | fzf --ansi --header '  CTRL-V Only 󰄬 / CTRL-X Only 󰅖 / CTRL-A All' --header-lines=1 --bind 'enter:become(mpv https://ok.ru/videoembed/{-1})+abort' --bind "ctrl-v:+transform-query(echo '󰄬 ')" --bind "ctrl-x:+transform-query(echo '󰅖 ')" --bind "ctrl-a:+transform-query(echo )" --preview "chafa --size=60% $IMG_PATH/{-1}" --preview-window 'nohidden,right,40%,border-left'
