ea3194b5918644d4966c02e30a40dff703c93423
Artemiy Misyurov <14artemiy88@gmail.com>
2024-05-03 21:19:33 +0500

    add params

--- booster.sh
+++ booster.sh
@@ -6,12 +6,10 @@ declare -r YELLOW_COLOR="\e[0;33m"
 declare -r BLUE_COLOR="\e[1;36m"
 declare -r RED_COLOR="\e[1;31m"
 declare -r GRAY_COLOR="\e[38;5;254m"
-declare -r IMG_PATH="/tmp/booster"
+declare -r IMG_PATH="$HOME/.cache/booster"
 
-bar_size=$(( $(tput cols) - 25 ))
-
-if [ ! -d $IMG_PATH ]; then
-	mkdir $IMG_PATH
+if [ ! -d "$IMG_PATH" ]; then
+	mkdir "$IMG_PATH"
 fi
 
 Help() {
@@ -19,63 +17,111 @@ Help() {
     echo "Get video from Boosty by channel name."
     echo
     echo -e "${YELLOW_COLOR}USAGE${NORMAL_COLOR}
-    booster CHANNEL"
+    booster CHANNEL [OPTIONS]"
     echo
     echo -e "${YELLOW_COLOR}ARGS${NORMAL_COLOR}
     ${GREEN_COLOR}<CHANNEL>${NORMAL_COLOR}    Channel name"
     echo
+    echo -e "${YELLOW_COLOR}OPTIONS${NORMAL_COLOR}
+    ${GREEN_COLOR}-v <string> ${NORMAL_COLOR}        Preview image viewer [default: chafa --size=60%]
+    ${GREEN_COLOR}--no-image ${NORMAL_COLOR}         Disable image preview"
+    echo
+}
+
+params() {
+    if [[ -z $1 ]]; then
+        show_help_and_exit
+    fi
+    if [ "$#" -gt 1 ]; then
+        if [[ "$2" == "--no-image" ]]; then
+            str=" ${YELLOW_COLOR}Free| Title\n"
+            SHOW_IMAGE=false
+        elif [[ "$2" == "-v" && -n "$3" ]]; then
+            IMAGE_VIEWER="$3"
+        else
+            show_help_and_exit
+        fi
+    fi
+}
+
+show_help_and_exit() {
+    Help
+    exit 0
 }
 
 load() {
-	if [ ! -f "$IMG_PATH/$vid" ]; then
-	
-		cha=" "
-		(( len=$bar_size - ${#title} ))
-    	local v=$(printf "%-${len}s" "$cha")
-    
-		echo -e '\e[2F\n'
-    	echo -en "Loading image...\t${GREEN_COLOR}$2${NORMAL_COLOR}${v// /$cha}"
-	
-		wget -O "$IMG_PATH/$vid" $1 &> /dev/null
+	if [[ ! -f "$IMG_PATH/$vid" ]]; then
+		local len=$((bar_size - ${#title}))
+    	readonly v=$(printf "%-${len}s" " ")
+    	echo -e '\e[2F\n'
+    	echo -en "Loading image...\t${GREEN_COLOR}$2${NORMAL_COLOR}${v// / }"
+		curl -s -o "$IMG_PATH/$vid" "$1" >/dev/null
 	fi
 }
 
-if [[ -z $1 ]]; then
-	Help
-	exit 0
-fi
-channel=$1
+function get_channels() {
+    readonly json=$(curl https://api.boosty.to/v1/blog/$1/media_album/\?type\=all\&limit_by\=media | jq -r '.data.mediaPosts')
+    local -i i=0
+    local title vid hasAccess access preview
+    while true; do
+    	title=$(jq -r ".[$i].post.title" <<< "$json")
+    	if [[ $title == null ]]; then
+    		break
+    	fi
+    	vid=$(jq -r ".[$i].media[0].vid" <<< "$json")
+    	if [[ $vid != "null" ]]; then
+    		hasAccess=$(jq -r ".[$i].post.hasAccess" <<< "$json")
+    		access="${RED_COLOR}󰅖${NORMAL_COLOR}"
+    		if [[ $hasAccess == true ]]; then
+    			access="${GREEN_COLOR}󰄬${NORMAL_COLOR}"
+    			preview=$(jq -r ".[$i].media[0].preview" <<< "$json")
+    		else
+    			preview=$(jq -r ".[$i].post.teaser[0].url" <<< "$json")
+    		fi
+    		str="$str\n $access|${BLUE_COLOR}$title${NORMAL_COLOR}"
+            if [ $SHOW_IMAGE = true ]; then
+    		    load "$preview" "$title"
+                fzf_params+=(--preview-window 'nohidden,right,40%,border-left')
+                fzf_params+=(--preview "$IMAGE_VIEWER $IMG_PATH/{-1}")
+    		    str="$str|${GRAY_COLOR}$vid${NORMAL_COLOR}"
+            else
+                fzf_params+=(--preview-window hidden)
+            fi
+    	fi
+    	((i++))
+    done
+}
 
-json=$(curl https://api.boosty.to/v1/blog/$channel/media_album/\?type\=all\&limit_by\=media | jq -r '.data.mediaPosts')
+SHOW_IMAGE=true
+str=" ${YELLOW_COLOR}Free| Title| Id${NORMAL_COLOR}\n"
+IMAGE_VIEWER="chafa --size=60%"
+params "${@}"
 
-i=0
-str=""
-while true; do
-	title=$(jq ".[$i].post.title" <<< $json | tr -d '"')
-	if [[ $title == null ]]; then
-		break;
-	fi
-	vid=$(jq ".[$i].media" <<< $json | jq '.[0].vid' | tr -d '"')
-	if [[ $vid -ne null ]]; then
-		hasAccess=$(jq ".[$i].post.hasAccess" <<< $json)
-		access="${RED_COLOR} 󰅖${NORMAL_COLOR}"
-		if [[ "$hasAccess" == true ]]; then
-			access="${GREEN_COLOR} 󰄬${NORMAL_COLOR}"
-			preview=$(jq ".[$i].media" <<< $json | jq '.[0].preview' | tr -d '"')
-		else
-			preview=$(jq ".[$i].post.teaser" <<< $json | jq '.[0].url' | tr -d '"')
-		fi
-		load $preview "$title"
+bar_size=$(( $(tput cols) - 25 ))
+fzf_params=(
+    "--ansi"
+    --header ' CTRL-V Only 󰄬 / CTRL-X Only 󰅖 / CTRL-A All'
+    "--header-lines=1"
+    --bind 'enter:become(mpv https://ok.ru/videoembed/{-1})+abort'
+    --bind "ctrl-v:+transform-query(echo '󰄬 ')"
+    --bind "ctrl-x:+transform-query(echo '󰅖 ')"
+    --bind "ctrl-a:+transform-query(echo )"
+    --bind "esc:+abort"
+)
 
-		str="$str\n $access|${BLUE_COLOR}$title${NORMAL_COLOR}|${GRAY_COLOR}$vid${NORMAL_COLOR}"
-	fi
-	((i++))
-done
+get_channels "$1"
 
 if [[ $str == "" ]]; then
 	echo "Nothing to show"
 	exit 0
 fi
 
-str="  ${YELLOW_COLOR}Free| Title| Id${NORMAL_COLOR}\n$str"
-echo -e "$str" | column --table --separator "|" | fzf --ansi --header '  CTRL-V Only 󰄬 / CTRL-X Only 󰅖 / CTRL-A All' --header-lines=1 --bind 'enter:become(mpv https://ok.ru/videoembed/{-1})+abort' --bind "ctrl-v:+transform-query(echo '󰄬 ')" --bind "ctrl-x:+transform-query(echo '󰅖 ')" --bind "ctrl-a:+transform-query(echo )" --preview "chafa --size=60% $IMG_PATH/{-1}" --preview-window 'nohidden,right,40%,border-left'
+echo -e "$str" | column --table --separator "|" | fzf "${fzf_params[@]}"
+
+
+
+
+
+
+
+
